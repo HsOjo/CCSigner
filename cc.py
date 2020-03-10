4caadcceb71b81b7501ebade90b3e9579ab7f19d
HsOjo <1134031392@qq.com>
2020-03-10 08:36:27 +0800

    Support custom host.

--- cc.py
+++ cc.py
@@ -8,32 +8,34 @@ class CC:
     STAT_SUCCESS = 1
     STAT_CLOSE = 2
 
-    LOGIN_URL = 'http://cc.szpt.edu.cn/Login.aspx'
-    SIGNIN_URL = 'http://cc.szpt.edu.cn/sSign.aspx'
+    LOGIN_URL = 'http://%s/Login.aspx'
+    SIGNIN_URL = 'http:/%s/cc.szpt.edu.cn/sSign.aspx'
 
-    def __init__(self):
+    def __init__(self, host='cc.szpt.edu.cn'):
         self._session = requests.session()
+        self.LOGIN_URL = self.LOGIN_URL % host
+        self.SIGNIN_URL = self.SIGNIN_URL % host
 
     def login(self, username, password):
-        resp = self._session.get(CC.LOGIN_URL)
+        resp = self._session.get(self.LOGIN_URL)
         fs = common.extract_forms(resp.text)
         if len(fs) > 0:
             f = fs[0]
             data = common.generate_data(f['inputs'])
             data['TextBoxTeacherName'] = username
             data['TextBoxPassword'] = password
-            resp = self._session.post(CC.LOGIN_URL, data)
+            resp = self._session.post(self.LOGIN_URL, data)
             return 'Logined' in resp.url
         else:
             return False
 
     def signin(self):
-        resp = self._session.get(CC.SIGNIN_URL)
+        resp = self._session.get(self.SIGNIN_URL)
         fs = common.extract_forms(resp.text)
         if len(fs) > 0:
             f = fs[0]
             data = common.generate_data(f['inputs'])
-            resp = self._session.post(CC.SIGNIN_URL, data)
+            resp = self._session.post(self.SIGNIN_URL, data)
             if '签到成功' in resp.text:
                 return CC.STAT_SUCCESS
             elif '签到未开通' in resp.text:
